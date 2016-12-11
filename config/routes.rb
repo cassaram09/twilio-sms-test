4ff5b8f28fbe12b96233ef685868842bedf71833
cassaram09 <cassaram09@gmail.com>
2016-12-11 16:01:21 -0500

    configure messages/reply route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  get 'messages/reply'
+  
+  resources :messages do 
+    collection do 
+      post 'reply'
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
