fb02ae1ec5fc3428e3543db807a8db8c0cbb5ecd
cassaram09 <cassaram09@gmail.com>
2016-12-11 16:13:17 -0500

    configure reply action to send response text via Twilio

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,23 @@
 class MessagesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
   def reply
+    message_body = params["Body"]
+    from_number = params["From"]
+    boot_twilio
+    sms = @client.messages.create(
+      from: Rails.application.secrets.twilio_number, 
+      to: from_number,
+      body: "Hello there, thanks for texting me. Your number is #{from_number}."
+    )
   end
+
+  private 
+
+  def boot_twilio
+    account_sid = ENV['TWILIO_SID']
+    auth_token = ENV['TWILIO_TOKEN']
+    @client = Twilio::REST::Client.new account_sid, auth_token
+  end
+
 end
