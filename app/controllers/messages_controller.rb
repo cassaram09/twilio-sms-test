a07ff5f1d9930ba68bc0521f01c5360394c66ceb
cassaram09 <cassaram09@gmail.com>
2016-12-11 16:36:05 -0500

    reconfigure ENV keys, secrets; twilio app works

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def reply
+
     message_body = params["Body"]
     from_number = params["From"]
     boot_twilio
